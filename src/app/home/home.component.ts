0a2f605dfc6bbc5fc1eca798bc5f40d104c8c62c
Danie van der Schyff <Danievds@velocitycubed.co.za>
2023-08-25 12:57:18 +0200

    start stop pause

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,16 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Observable, Subscription, timer } from 'rxjs';
+import { Observable, Subscription, timer, fromEvent, interval, merge, EMPTY } from 'rxjs';
+import {
+  takeWhile,
+  takeUntil,
+  switchMap,
+  scan,
+  startWith,
+  repeat,
+  map,
+  tap,
+  filter
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -16,19 +27,67 @@ export class HomeComponent implements AfterViewInit, OnInit {
   @ViewChild('gameboard', { static: false })
   canvas!: ElementRef<HTMLCanvasElement>;
 
-    private ctx!: CanvasRenderingContext2D;
+  private ctx!: CanvasRenderingContext2D;
+
+  @ViewChild('start') start!: ElementRef<HTMLButtonElement>;
+  @ViewChild('stop') stop!: ElementRef<HTMLButtonElement>;
+  @ViewChild('pause') pause!: ElementRef<HTMLButtonElement>;
+
+  counterDisplayHeader = document.querySelector('h3');
+
+  startClick$! : Observable<any>;
+  stopClick$! : Observable<any>;
+  pauseBtn$! : Observable<any>;
+
+
+
+
   ngOnInit(): void {
-this.subscription = this.everyFiveSeconds.subscribe(() => {
-             this.drawRect();
-       });
+    // this.subscription = this.everyFiveSeconds.subscribe(() => {
+    //             this.drawRect();
+    // });
+
+
+
+
   }
+
+
 
   ngAfterViewInit(): void {
           this.ctx = this.canvas.nativeElement.getContext('2d')!;
     this.canvas.nativeElement.width = 1000;
     this.canvas.nativeElement.height = 1000;
+
+  this.startClick$ = fromEvent(this.start.nativeElement, 'click');
+    this.stopClick$ = fromEvent(this.stop.nativeElement, 'click').pipe(tap(() => {
+
+      this.ctx.clearRect(0, 0, 1000, 1000);
+    }));
+    this.pauseBtn$ = fromEvent(this.pause.nativeElement, 'click');
+
+
+
     console.log("pop")
-    this.drawRect();
+
+        merge(
+      this.startClick$.pipe(map(()=>true)),
+      this.pauseBtn$.pipe(map(()=>false))
+    )
+      .pipe(
+        switchMap(val => (val ? interval(10) : EMPTY)),
+        scan((acc: number, curr: number) => acc + curr, 0),
+        takeWhile(val => val >= 0),
+        startWith(0),
+        takeUntil(this.stopClick$),
+        repeat(),
+        filter(val=>val >0)
+      )
+      .subscribe(val => {
+        this.drawRect();
+      });
+
+
 
     }
 
